5c13ac8413b4a9885dc86e6f78b41cadb6dd06d1
uummoo <levin.eric.zimmermann@posteo.de>
2020-04-06 23:34:04 +0200

    enhanched infit

--- mu/utils/infit.py
+++ mu/utils/infit.py
@@ -7,6 +7,8 @@ import operator
 
 
 class InfIt(abc.ABC):
+    """Abstract class for infinite generators."""
+
     @abc.abstractmethod
     def __next__(self) -> object:
         raise NotImplementedError
@@ -15,6 +17,10 @@ class InfIt(abc.ABC):
 class Cycle(InfIt):
     def __init__(self, iterable: tuple):
         self.__cycle = itertools.cycle(iterable)
+        self.__repr = "Cycle({})".format(iterable)
+
+    def __repr__(self) -> str:
+        return self.__repr
 
     def __next__(self) -> object:
         return next(self.__cycle)
@@ -25,6 +31,9 @@ class MathOperation(InfIt):
         self.__operator = operator
         self.__value = start
         self.__n = n
+
+    def __repr__(self) -> str:
+        return "InfitMath({}, {}, {})".format(self.__value, self.__n, self.__operator)
 
     def __next__(self) -> object:
         new_value = self.__operator(self.__n, self.__value)
@@ -43,7 +52,7 @@ class Multiplication(MathOperation):
 
 
 class Power(MathOperation):
-    def __init__(self, start: float = 1, n: float = 2):
+    def __init__(self, start: float = 2, n: float = 2):
         super().__init__(lambda n, value: value ** n, start, n)
 
 
@@ -57,3 +66,16 @@ class Random(InfIt):
     @property
     def random_module(self):
         return self.__random_module
+
+
+class Uniform(Random):
+    def __init__(self, seed: int = 1, lower_border: float = 0, upper_border: float = 1):
+        super().__init__(seed)
+        self.__lower_border = lower_border
+        self.__uppper_border = upper_border
+
+    def __repr__(self) -> str:
+        return "Uniform({}, {})".format(self.__lower_border, self.__uppper_border)
+
+    def __next__(self) -> float:
+        return self.random_module.uniform(self.__lower_border, self.__uppper_border)
